4babe75298ac3fd89329bc1e7233dd48cb3daf8c
Marian <marian@becher-home.net>
2020-01-31 19:20:48 +0100

    initial stuff

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Character : MonoBehaviour
+{
+    private float moveSpeed;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void _Move()
+    {
+
+    }
+}
