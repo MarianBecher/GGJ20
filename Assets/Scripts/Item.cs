0114ec646d64d14535d2ed40033a4a21132fda23
Tom Witkowski <dev.gummibeer@gmail.com>
2020-01-31 20:36:05 +0100

    add item and item type

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+	public ItemType type;
+
+	public float timeToDecay = 10.0f;
+
+    void Start()
+    {
+        Destroy(gameObject, timeToDecay);
+    }
+}
---
+++ Assets/Scripts/ItemType.cs
@@ -0,0 +1,13 @@
+﻿public enum ItemType
+{
+    Head = 0,
+    LeftArm = 1,
+    LeftHand = 2,
+    RightArm = 3,
+    RightHand = 4,
+    Torso = 5,
+    LeftLeg = 6,
+    LeftFoot = 7,
+    RightLeg = 8,
+    RightFoot = 9
+}
