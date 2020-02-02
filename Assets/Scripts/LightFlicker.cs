1a14ce5e92c6c019765829382098b06e9de31577
flash-miller <51022442+flash-miller@users.noreply.github.com>
2020-02-02 15:18:00 +0100

    Light bitcheees

---
+++ Assets/Scripts/LightFlicker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityEngine.Experimental.Rendering.Universal
+{
+    public class LightFlicker : MonoBehaviour
+    {
+        [SerializeField] AnimationCurve flickerCurve;
+        [SerializeField] float baseIntensity;
+        float randOffset;
+        Light2D flickeringLight;
+        float timeCounter = 0.0f;
+
+        void Start(){
+            randOffset = Random.Range(0.0f, 1.0f);
+            flickeringLight = GetComponentInChildren<Light2D>();
+        }
+
+        void Update()
+        {
+            flickeringLight.intensity = baseIntensity * flickerCurve.Evaluate(timeCounter+randOffset);
+            timeCounter += Time.unscaledDeltaTime * 0.5f;
+            timeCounter %= 1.0f;
+        }
+    }
+}
