6e23739a2a7f2300eb34ad99ad285fe922ae3b9d
Marian <marian@becher-home.net>
2020-02-02 09:17:51 +0100

    draft of monster complete animation

--- Assets/Scripts/WorkingBench.cs
+++ Assets/Scripts/WorkingBench.cs
@@ -136,6 +136,7 @@ public class WorkingBench : Interactable
             _interactingCharacter.disableMovement = false;
             OnBodyCompleted?.Invoke();
             AudioManager.Instance.Play("MonsterAlive");
+            GetComponent<Animator>().SetTrigger("Complete");
             _CreateNewBody();
         }
     }
